4f5a82354d237bffe8218b7895f10faf78cc4d83
Gerardo Hernandez Flores <g.hernandez@kapital.mx>
2023-10-24 17:18:17 -0600

    Working with Route Handlers

---
+++ src/app/api/users/route.js
@@ -0,0 +1,22 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json()
+    return NextResponse.json({data})
+}
+export function POST() {
+    return NextResponse.json({
+        message: 'creando datos!'
+    })
+}
+export function PUT() {
+    return NextResponse.json({
+        message: 'actualizando dato!'
+    })
+}
+export function DELET() {
+    return NextResponse.json({
+        message: 'eliminando datos!'
+    })
+}
