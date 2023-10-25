640430622d3384e76a65fd12987225dc10b20fa2
Gerardo Hernandez Flores <g.hernandez@kapital.mx>
2023-10-25 12:24:22 -0600

    working with searchParams

--- src/app/api/users/[userId]/route.js
+++ src/app/api/users/[userId]/route.js
@@ -2,7 +2,11 @@ import second, { NextResponse } from 'next/server';
 
 export async function GET( request, {params} ){
 
-    console.log(request)
+    const { searchParams } = new URL(request.url);
+    console.log(searchParams);
+
+    console.log(searchParams.get('nombre'));
+    console.log(searchParams.get('apellido'));
 
     const res = await fetch(`https://jsonplaceholder.typicode.com/users/${ params.userId }`)
     const data = await res.json()
